9668f2747826d5740390e7fa5f17212ba780d284
Abdul <abdul@Abduls-MacBook-Air.local>
2025-07-19 20:23:13 +0530

    this is new branch created for changes in studentservice

--- src/main/java/net/spring/abdul/student/studentservice/entity/StudentDetails.java
+++ src/main/java/net/spring/abdul/student/studentservice/entity/StudentDetails.java
@@ -1,5 +1,10 @@
 package net.spring.abdul.student.studentservice.entity;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,30 +15,38 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "student_details")
 public class StudentDetails {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(name = "student_id",nullable = false)
+	@NotNull(message = "student id not a null value ")
+	@Column(name = "student_id", nullable = false, unique = true)
+	@Min(value = 1, message = "value is greater than zero")
 	private int studentId;
-	@Column(name = "student_name",nullable = false)
+	@NotBlank(message = "Stundent name should not be blank")
+	@Size(max = 100, message = "studnet name must be 100 characters only")
+	@Column(name = "student_name", nullable = false)
 	private String studentName;
+	@Min(message = "age must be positive ", value = 0)
+	@Max(message = "Age must be relastick", value = 150)
 	@Column(name = "student_age")
 	private int studentAge;
 
-	
+	// Default constructor
 	public StudentDetails() {
-		
+
 	}
-	
-	public StudentDetails(Long id,int studentId,String studentName,int studentAge) {
+
+	// Parameterized constructor
+	public StudentDetails(Long id, int studentId, String studentName, int studentAge) {
 		this.setId(id);
 		this.setStudentId(studentId);
 		this.setStudentName(studentName);
 		this.setStudentAge(studentAge);
 	}
-	
-	
+
+	// Getters and Setters
+
 	public Long getId() {
 		return id;
 	}
